f88e84722217a5e78ffc1547dc0c2781f4d06367
shreyas-desai <shreyasdesai3013@gmail.com>
2023-11-18 12:15:40 -0500

    update adventure.py

--- adventure.py
+++ adventure.py
@@ -2,17 +2,40 @@ import argparse
 import sys
 import json
 
+def look(current_room,data):
+    print(f"> {(data[current_room]['name'])}")
+    print()
+    print(data[current_room]['desc'])
+    print()
+    if 'items' in data[current_room].keys() and len(data[current_room]['items'])!=0:
+        print(f"Items:",*data[current_room]['items'])
+        print()
+    print(f"Exits:",*data[current_room]['exits'].keys())
+    print()
+    
+
+
+
 def parse_map(map_file):
     data = json.load(map_file)
-    print(f"> {(data[0]['name'])}")
-    print()
-    print(data[0]['desc'])
-    print()
-    print(f"Exits:",*data[0]['exits'].keys())
-    print()
+    current_room = 0
+    look(current_room,data)
+    current_room_data = data[current_room]
     while True:
-        print("What would you like to do?")
-        break
+        current_room_data = data[current_room]
+        direction = input("What would you like to do? ")
+        if 'go' in direction.lower().split(' ')[0]:
+            try:
+                if direction.lower().split(' ')[1] in current_room_data['exits']:
+                    print(f"You go {direction.lower().split(' ')[1].lower()}")
+                    current_room = current_room_data['exits'][direction.lower().split(' ')[1]]
+                    print(current_room)
+                    look(current_room,data)
+                else:
+                    print(f"There's no way to go {direction.lower().split(' ')[1].lower()}")
+                
+            except:
+                print("Sorry you need to go somewhere")
 
 def main():
     parser = argparse.ArgumentParser(description = 'Get the map file to start playing!')
