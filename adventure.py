3f18461e9bc2698641222bf86e15cf067fe32bb2
shreyas-desai <shreyasdesai3013@gmail.com>
2023-11-22 02:54:06 -0500

    updates

--- adventure.py
+++ adventure.py
@@ -3,15 +3,11 @@ import sys
 import json
 
 def look(current_room,data):
-    print(f"> {(data[current_room]['name'])}")
-    print()
-    print(data[current_room]['desc'])
-    print()
+    print(f"> {(data[current_room]['name'])}\n")
+    print(f"{data[current_room]['desc']}\n")
     if 'items' in data[current_room].keys() and len(data[current_room]['items'])!=0:
-        print(f"Items:",*data[current_room]['items'])
-        print()
-    print(f"Exits:",*data[current_room]['exits'].keys())
-    print()
+        print(f"Items: {' '.join(data[current_room]['items'])}\n")
+    print(f"Exits: {' '.join(data[current_room]['exits'].keys())}\n")
 
 
 def parse_map(map_file):
@@ -29,14 +25,17 @@ def parse_map(map_file):
             if 'go' in verb.split(' ')[0]:
                 try:
                     exit = verb.split(' ')[1]
+
+                    assert exit != ''
+
                     if exit in current_room_data['exits']:
-                        print(f"You go {exit}")
+                        print(f"You go {exit}.\n")
                         current_room = current_room_data['exits'][exit]
                         look(current_room,data)
                     else:
-                        print(f"There's no way to go {exit}")
+                        print(f"There's no way to go {exit}.")
                 except:
-                    print("Sorry you need to go somewhere")
+                    print("Sorry, you need to 'go' somewhere.")
             
             if 'look' in verb:
                 try:
@@ -53,16 +52,17 @@ def parse_map(map_file):
                     else:
                         print(f"There's no {item} anywhere.")
                 except:
-                    print("You need to 'get' something!")
+                    print("Sorry, you need to 'get' something.")
                         
             
             if 'inventory' in verb:
                 try:
+                    assert len(items)!=0
                     print("Inventory:")
                     for item in items:
-                        print(f"\t{item}")
+                        print(f"  {item}")
                 except:
-                    print("You're not carrying anything")
+                    print("You're not carrying anything.")
         except EOFError:
             print("Use 'quit' to exit.")
 
